d00a90665d1520f40810e4b7e46ed6b3b41b8e3e
Ivan <ivapie@gmail.com>
2019-02-09 13:19:20 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Global Dependencies 
+
+// JS - Node Modules and Custom Libs
+import 'jquery/dist/jquery.min.js'
+import 'popper.js/dist/umd/popper.min.js'
+import 'bootstrap/dist/js/bootstrap'
+
+// CSS - Node Modules and Custom Libs
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.scss'
